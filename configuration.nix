b35931db05255b8cedb0dc949f741759b75600e1
kukus <kukus@leHovno>
2025-11-11 12:52:06 +0100

    basics

--- configuration.nix
+++ configuration.nix
@@ -1,8 +1,26 @@
 { config, pkgs, ... }:
 
 {
-  # Základní kernel moduly potřebné pro CNI a overlay FS
+  imports = [
+    ./hardware-configuration.nix
+  ];
   boot.kernelModules = [ "overlay" "br_netfilter" ];
+
+  boot.loader.grub.device = "/dev/nvme0n1";
+
+  networking.hostName = "nixos-k8s-cp";
+
+  users.users.kukus888 = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" "networkmanager" ];
+  };
+
+  time.timeZone = "Europe/Prague"; 
+
+  # Enable wayland
+  services.xserver.enable = true;
+  services.xserver.displayManager.sddm.enable = true;
+  services.xserver.desktopManager.plasma5.enable = true;
 
   # Sysctl pro Kubernetes
   networking.sysctl = {
@@ -24,6 +42,10 @@
     openssl
     # doporučené pro ladění eBPF/Cilium
     bpftool
+    # tools
+    tmux
+    git
+    vim
   ];
 
   # Zapnout containerd službu (kubelet povolíme po rebuildu systémově)
